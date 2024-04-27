c274719daca83497fceaf0faa0d02f96b376bf43
handue <124148074+handue@users.noreply.github.com>
2024-04-28 04:09:21 +0900

    welcomeSection

--- HTML&CSS&JS-NETFLIX/js/main.js
+++ HTML&CSS&JS-NETFLIX/js/main.js
@@ -1,8 +1,34 @@
-console.log('gd');
+/**
+ * Section = Welcome Part 
+ */
 
-var emailInput = document.querySelector('#email')
-console.log(emailInput);
+var emailInput = document.querySelector('#email');
+var emailPlaceholder = document.querySelector('.email-placeholder');
 
-if(emailInput !=''){
-    
+
+emailInput.addEventListener('input', ()=>{ 
+    console.log('이메일 값 변경됨', emailInput.value);
+    if(emailInput.value)
+    textCheck();
+});
+
+emailInput.addEventListener('focus',function(){
+    emailPlaceholder.classList.add('active');
+});
+
+emailInput.addEventListener('blur',function(){
+    if(emailInput.value ==''){
+    emailPlaceholder.classList.remove('active');
+    }
+});
+
+
+function textCheck(){
+    if(emailInput.value !=''){
+        emailPlaceholder.classList.add('active');
+    }else{
+        emailPlaceholder.classList.remove('active');
+    }
 }
+
+// 만약 input 내용이 비어있지 않다면, active클래스 추가. 비어있으면 active 클래스 제거 
