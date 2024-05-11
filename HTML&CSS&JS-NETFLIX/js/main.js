4a8657da1f9b8b166d81dab826c492743e106ba1
handue <124148074+handue@users.noreply.github.com>
2024-05-11 23:17:21 +0900

    js로는 가상 클래스 조작이 어렵다 수정해야함

--- HTML&CSS&JS-NETFLIX/js/main.js
+++ HTML&CSS&JS-NETFLIX/js/main.js
@@ -9,12 +9,13 @@ var emailPlaceholder2 = document.querySelector('.email-placeholder2')
 // var faqList = document.querySelectorAll('question-list li')
 var faqList = $('.question-list li:not(.active)');
 
+// todo: 로그인 모달 Remember me 버튼
+
+
+
 
 placeholderEffect(emailInput,emailPlaceholder);
 placeholderEffect(emailInput2,emailPlaceholder2);
-
-
-
 
 for(let i=0; i<=5; i++){
     let clickCheck = false;
@@ -49,7 +50,7 @@ for(let i=0; i<=5; i++){
 // 음 뭐, hover는 제이쿼리 3.3버전부터 삭제? 돼서 요즘은 mouseenter mouseleave 쓴다네. 근데 지금은 자바 스크립트 공부하려고 한거긴 한데 그냥 CSS 돌리는게 더 편함 ㅋㅋ
 // 이벤트핸들러의 경우 i는 전역변수를 참조하게 됨. for문으로 함수를 여러개 돌릴떄 사실상 같은 함수가 여러개 만들어지는건데, let은 블록이 스코프인 반면 var은 전역으로 사용 가능하잖아. 그렇게 됐을 때 for문에 있는 i의 값은 각각 가져가지만, 이벤트 핸들러의 경우에는 가장 마지막에 재선언된 var의 값을 가져간대. 그래서 this가 아니라 faqList.eq(i).css로 할 경우 i의 마지막 값만 바뀜. 그래서 이럴땐 var로 할거면 이벤트 핸들러를 this로 해주거나 let으로 포문 돌려야 할듯.
 
-// TODO : 함수 모음  
+// ! 함수 모음  
 
 function placeholderEffect(selectInput,selectPlaceholder){
     selectInput.addEventListener('input', ()=>{ 
